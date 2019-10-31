79742ff78a1b22f7fd082cc070467622301a463b
Lestar039 <49066114+Lestar039@users.noreply.github.com>
2019-10-31 18:09:33 +0300

    update cross links

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+# class MainPageLocators:
+#     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators:
@@ -16,3 +16,12 @@ class BasketButtonLocators:
     PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, ".alertinner > strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, ".alertinner > p > strong")
+
+
+class ProductPageLocators:
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    # LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import BasketButtonLocators
+from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
@@ -10,6 +11,7 @@ class ProductPage(BasePage):
         self.should_be_correct_product_name()
         self.should_be_correct_price()
 
+    # Метод, добавляющий товар в корзину
     def should_be_add_product_to_basket(self):
         basket_button = self.browser.find_element(*BasketButtonLocators.BASKET_BUTTON)
         basket_button.click()
@@ -17,15 +19,28 @@ class ProductPage(BasePage):
     # def should_be_product_link(self):
     #     assert "?promo=newYear" in self.browser.current_url, "'?promo=newYear' is not presented"
 
+    # Проверка наличия кпонки "Положить в корзину"
     def should_be_basket_button(self):
         assert self.is_element_present(*BasketButtonLocators.BASKET_BUTTON), "Basket button is not presented"
 
+    # Проверка совпадения названия товара с названием во всплывающем сообщении
     def should_be_correct_product_name(self):
         product_name = self.browser.find_element(*BasketButtonLocators.PRODUCT_NAME)
         product_in_basket = self.browser.find_element(*BasketButtonLocators.PRODUCT_NAME_IN_BASKET)
         assert product_name.text == product_in_basket.text, "Product name is not correct"
 
+    # Проверка совпадения цены товара и цены во всплывающем сообщении
     def should_be_correct_price(self):
         product_price = self.browser.find_element(*BasketButtonLocators.PRODUCT_PRICE)
         price_in_basket = self.browser.find_element(*BasketButtonLocators.PRODUCT_PRICE_IN_BASKET)
         assert product_price.text == price_in_basket.text, "Price is not correct"
+
+    # Метод, который проверяет, чтобы элемент не появлялся на странице в течение заданного времени
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    # Метод, который проверяет, чтобы элемент исчезал на странице в течение заданного времени
+    def should_be_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message not is presented, but should be"
